8917feb9ea755d7a091ea24fa622371079899739
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-01-18 23:26:36 +0200

    feat: define endpoints and initialize the connection class with get method

---
+++ lib/core/connection.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:user_profile_management/model/dio_response.dart';
+
+class Connection {
+  static final Dio _dio = Dio();
+  static Connection? _connectionInstance;
+
+  Connection._();
+
+  static Connection get instance => _connectionInstance ??= Connection._();
+
+  Future<DioResponse> get({required String url}) async {
+    try {
+      final response = await _dio.get(url);
+      // SharedPreference.setString(
+      //     key: "users", value: jsonEncode(response.data));
+      return DioResponse.success(response.data);
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 409) {
+        return DioResponse.failure(
+            'Conflict: The request conflicts with the server state.');
+      }
+      return DioResponse.failure(e.message ?? "DioException occurred");
+    } catch (e) {
+      return DioResponse.failure('An unexpected error occurred: $e');
+    }
+  }
+}
